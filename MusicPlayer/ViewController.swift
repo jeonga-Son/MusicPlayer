341cd2ee7b970ea673216a83fc02fb2047560b2e
jeonga-Son <jjeong0106@gmail.com>
2022-10-05 10:03:22 -0400

    인터페이스 빌더의 객체를 코드와 연결(IBTOutlet)

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // user interface와 연결할 property를 만들어준다.
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLable: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
